0cc45efe18d887f70e18bdaae795e7f03e0b78a7
Karen Bu <ktianbu@gmail.com>
2025-10-15 02:58:03 +0000

    working on calendar page, added calendar asset. pushing in order to merge and pull from main

--- client/src/1_Pages/Calendar.tsx
+++ client/src/1_Pages/Calendar.tsx
@@ -1,7 +1,27 @@
+import { DateCalendar } from "@mui/x-date-pickers";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+
+import { useState } from "react";
+
+import dayjs from "dayjs";
+
 export function Calendar() {
+  const [date, setDate] = useState(dayjs())
+
   return (
-    <div className="content-page">
-      <h1>This is where the calendar will go!</h1>
+    <div className="content-page calendar">
+      <div className='calendar row-100'>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DateCalendar value={dayjs()} onChange={() => setDate(date)} />
+        </LocalizationProvider>
+      </div>
+      <div className='calendar row-100'>
+
+      </div>
+      <div className='calendar row-100 text'>
+        <h1>Here's what you've done this week!</h1>
+      </div>
     </div>
   );
 }
