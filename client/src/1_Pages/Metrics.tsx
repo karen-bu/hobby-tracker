23a9a2603e8f5ad4052a1a913039ae82f3d28473
Karen Bu <ktianbu@gmail.com>
2025-10-15 01:49:50 +0000

    imported material ui date picker

--- client/src/1_Pages/Metrics.tsx
+++ client/src/1_Pages/Metrics.tsx
@@ -1,7 +1,9 @@
 export function Metrics() {
   return (
-    <div className="content-page">
-      <h1>This is where metrics will go!</h1>
+    <div className="content-page metrics">
+      <div className="metrics row-100">
+        <h1>This is where metrics will go!</h1>
+      </div>
     </div>
   );
 }
